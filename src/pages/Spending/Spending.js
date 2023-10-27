811d969daa4e3ee2e1e9cd602a1d2d50703fcedd
Koleton Murray <106117059+koletonmurray@users.noreply.github.com>
2023-10-27 10:55:07 -0600

    added subtraction icons

--- src/pages/Spending/Spending.js
+++ src/pages/Spending/Spending.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
+// Import money pngs
+import sub1d from '../../img/sub1d.png'
+import sub5d from '../../img/sub5d.png'
+import sub10d from '../../img/sub10d.png'
+import sub20d from '../../img/sub20d.png'
+import sub1c from '../../img/sub1c.png'
+import sub5c from '../../img/sub5c.png'
+import sub10c from '../../img/sub10c.png'
+import sub25c from '../../img/sub25c.png'
+
 function Spending() {
   return (
     <p>
